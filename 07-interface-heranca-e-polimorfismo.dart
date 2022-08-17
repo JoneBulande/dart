975784a9e890c67e4304d451b4c1b2a0f083adb8
Jone Bulande <JoneProgrammer@gmail.com>
2022-08-17 19:03:41 +0200

    07 - Interfaces, Herança e Polimorfismo

--- 07-interface-heranca-e-polimorfismo.dart
+++ 07-interface-heranca-e-polimorfismo.dart
@@ -1,5 +1,50 @@
-void main() {}
+void main() {
+  // print(Man.talk());
 
-abstract class Pessoa {
-  String comunicar();
+  Kid kid = Kid();
+  print(kid.talk());
+
+  Payment payment = payWithMpesa();
+  payment.pay();
+  payment = payWithPayPal();
+  payment.pay();
+}
+
+abstract class Person {
+  bool single = true;
+}
+
+class Man implements Person {
+  // abstração "prop - obrigatória"
+  bool single = true;
+}
+
+class Women implements Person {
+  // abstração "prop - obrigatória"
+  bool single = false;
+}
+
+class Parent {
+  String talk() {
+    return 'Hello...';
+  }
+}
+
+// Herança
+class Kid extends Parent {}
+
+abstract class Payment {
+  void pay();
+}
+
+class payWithMpesa implements Payment {
+  void pay() {
+    print('Pay with M-Pesa');
+  }
+}
+
+class payWithPayPal implements Payment {
+  void pay() {
+    print('Pay with Paypal');
+  }
 }
