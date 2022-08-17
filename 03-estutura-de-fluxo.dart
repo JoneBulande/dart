753b21de6a3adb8ce5a95621dd50c516bce106e5
Jone Bulande <JoneProgrammer@gmail.com>
2022-08-17 02:37:54 +0200

    03 - Estruturas de Fluxo

---
+++ 03-estutura-de-fluxo.dart
@@ -0,0 +1,27 @@
+void main() {
+  bool seguirEmFrente = true;
+  if (seguirEmFrente) {
+    print('Andar');
+  } else {
+    print('Parar');
+  }
+
+  if (20 > 18) {
+    print('20 é maior que 18');
+  }
+
+  int valor = 4;
+  switch (valor) {
+    case 0:
+      print('zero');
+      break;
+    case 1:
+      print('um');
+      break;
+    case 2:
+      print('Dois');
+      break;
+    default:
+      print('padrao');
+  }
+}
