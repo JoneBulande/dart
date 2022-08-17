104c228767cc0e201cd0290f9e45f106aaa3a0c9
Jone Bulande <JoneProgrammer@gmail.com>
2022-08-17 02:44:40 +0200

    04 - Estruturas de Repetição

---
+++ 04-estutura-de-repeticao.dart
@@ -0,0 +1,11 @@
+void main() {
+  for (var i = 0; i < 10; i++) {
+    print(i * 2);
+  }
+
+  int contador = 0;
+  while (contador <= 10) {
+    print('contador: ${contador}');
+    contador++;
+  }
+}
